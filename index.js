9baff40f5116b9664c4567a18142863658786565
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-06-14 11:52:48 -0400

    continued designing main body, played around nav

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// AOS Script Init
 AOS.init({
     offset: 120, // offset (in px) from the original trigger point
     delay: 100, // values from 0 to 3000, with step 50ms
@@ -6,3 +7,7 @@ AOS.init({
     once: true, // whether animation should happen only once - while scrolling down
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
+
+
+
+
