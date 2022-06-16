ec2ff7024f6367e0dd142a8111ef91a2babfc1c3
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-06-16 10:32:49 -0400

    change footer, link rest of the APIs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const { nextTick } = require("process");
 
 // set ejs view engine
 app.set("view engine", "ejs");
@@ -8,24 +9,76 @@ app.set("view engine", "ejs");
 // sends static html/css/js files
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
-  // request Kanye REST API id="kanyeApiText"
-  https.get('https://api.kanye.rest/', (response) => {
+// API get
+app.get("/", getKanye, getAnime, getInspire, getProgram, renderForm);
+  // response Kanye REST API id="kanyeQuote"
+  function getKanye(req, res, next){
+    https.get('https://api.kanye.rest/', (response) => {
     response.on('data', (data) => {
-      let quote = JSON.parse(data).quote;
-      console.log("Quote of the Day: " + quote);
-      res.render("index", {kanyeQuote: quote});
+      let kanyeQuote = JSON.parse(data).quote;
+      console.log("Quote of the Day: " + kanyeQuote);
+      res.locals.savedKanyeQuote = kanyeQuote;
+      next();
     });
     }).on('error', (e) => {
       console.error(e);
-  });
+    });
+  };
 
-  // https://animechan.vercel.app/api/random
+  // response AnimeQuotes API id=""
+  function getAnime(req, res, next){
+    https.get('https://animechan.vercel.app/api/random', (response) => {
+    response.on('data', (data) => {
+      let animeQuote = JSON.parse(data).quote;
+      let fromCharacter = JSON.parse(data).character;
+      let fromAnime = JSON.parse(data).anime;
 
-});
+      res.locals.savedAnimeQuote = animeQuote;
+      res.locals.savedFromCharacter = fromCharacter;
+      res.locals.savedFromAnime = fromAnime; 
+      next();
+    });
+    }).on('error', (e) => {
+      console.error(e);
+    });
+  };
 
+  // response InspirationQuotes API id=""
+  function getInspire(req, res, next){
+    https.get('https://api.goprogram.ai/inspiration', (response) => {
+    response.on('data', (data) => {
+      let inspireQuote = JSON.parse(data).quote;
+      let inspireAuthor = JSON.parse(data).author;
+    
+      res.locals.savedInspireQuote = inspireQuote;
+      res.locals.savedInspireAuthor = inspireAuthor;
+      next();
+    });
+    }).on('error', (e) => {
+      console.error(e);
+    });
+  }
 
-
+  // response ProgrammingQuotes API id=""
+  function getProgram (req, res, next){
+    https.get('https://programming-quotes-api.herokuapp.com/quotes/random', (response) => {
+    response.on('data', (data) => {
+      let programQuote = JSON.parse(data).en;
+      let programAuthor = JSON.parse(data).author;
+    
+      res.locals.savedProgramQuote = programQuote;
+      res.locals.savedProgramAuthor = programAuthor;
+      next();
+    });
+    }).on('error', (e) => {
+      console.error(e);
+    });
+  }
+  
+  function renderForm(rem, res) {
+    res.render("index");
+  };
+ 
 // Heroku Port Configuration
 var port = process.env.PORT;
 
